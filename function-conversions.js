b0c26f385b31c84bfc6e8fc02e3fe81be8e318ef
rileyrohloff <rileyrohloff@workfront.com>
2019-06-25 12:59:07 -0600

    updated es5 to es6 syntax for given functions

---
+++ function-conversions.js
@@ -0,0 +1,45 @@
+// Take the commented ES5 syntax and convert it to ES6 arrow Syntax
+
+// let myFunction = function () {
+// console.log("Function was invoked!");
+// };
+
+let myFunction = () => {
+    console.log('Function was invoked')
+}
+
+myFunction();
+
+// let anotherFunction = function (param) {
+//   return param;
+// };
+const anotherFunction = (arg) => {
+    console.log(arg)
+}
+anotherFunction("Example");
+
+// let add = function (param1, param2) {
+//   return param1 + param2;
+// };
+let add = (param1, param2) => {
+    console.log(param1 + param2)
+}
+add(1, 2);
+
+// let subtract = function (param1, param2) {
+//   return param1 - param2;
+// };
+let subtract = (param1, param2) => {
+    return param1 - param2;
+};
+
+subtract(1, 2);
+// Stretch
+
+exampleArray = [1, 2, 3, 4];
+// const triple = exampleArray.map(function (num) {
+//   return num * 3;
+// });
+
+const triple = exampleArray.map(x => x * 3)
+console.log(triple);
